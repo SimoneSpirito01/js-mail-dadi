c618260fefbba5a94203f9f100069d191f7cc822
SimoneSpirito <simospiri01@gmail.com>
2021-11-10 17:03:38 +0100

    gioco dei dadi

--- js/script.js
+++ js/script.js
@@ -10,7 +10,31 @@ for (let i = 0; i < listaMail.length; i++){
 }
 
 if (sentinella){
-    console.log('accesso consentito');
+    alert('accesso consentito');
+    const titolo = document.createElement('h1');
+    titolo.innerHTML = "Gioco dei Dadi";
+    document.querySelector('body').prepend(titolo);
+    const btn = document.createElement('button');
+    btn.innerHTML = "Gioca";
+    document.querySelector('body').append(btn);
+    
+    btn.addEventListener('click', function(){
+        const rdComputer = Math.floor(Math.random() * 6) + 1;
+        const rdUser = Math.floor(Math.random() * 6) + 1;
+        document.querySelector('.numero-pc').innerHTML = "Dado del Computer: " + rdComputer;
+        document.querySelector('.numero-user').innerHTML = "Dado dell'utente: " + rdUser;
+
+        if (rdComputer > rdUser){
+            document.querySelector('.vincitore').innerHTML = "Il vincitore è il computer!";
+        }
+        else if (rdComputer == rdUser) {
+            document.querySelector('.vincitore').innerHTML = "Nessun vincitore, pareggio!";
+        }
+        else {
+            document.querySelector('.vincitore').innerHTML = "Il vincitore è l'utente!";
+        }
+    })
+
 } else {
-    console.log('accesso non consentito')
+    alert('accesso non consentito')
 }
